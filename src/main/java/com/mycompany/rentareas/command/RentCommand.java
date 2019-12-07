9eb31c3a3873defbc85b098cd3c54fbb2f5d8ce0
Kumaisu <sugi.chama.1119@ezweb.ne.jp>
2019-12-07 22:41:18 +0900

    List Update
    基本的なリスト表示は完了
    オプションフラグ等は後検討

--- src/main/java/com/mycompany/rentareas/command/RentCommand.java
+++ src/main/java/com/mycompany/rentareas/command/RentCommand.java
@@ -5,7 +5,6 @@
  */
 package com.mycompany.rentareas.command;
 
-import java.util.UUID;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -61,7 +60,7 @@ public class RentCommand implements CommandExecutor {
                 case "reload":
                     ConfigManager.load();
                     Tools.Prt( player, ( "Rental Areas Config Reloaded." ), Config.programCode );
-                    return true;
+                    break;
                 case "console":
                     if ( !Tools.setDebug( args[1], Config.programCode ) ) {
                         Tools.entryDebugFlag( Config.programCode, Tools.consoleMode.normal );
@@ -73,53 +72,57 @@ public class RentCommand implements CommandExecutor {
                         ChatColor.GREEN + " ]",
                         Config.programCode
                     );
-                    return true;
+                    break;
                 case "status":
                     ConfigManager.Status( player );
-                    return true;
+                    break;
+                case "vacancy":
+                    DataList.List( player, -1 );
+                    break;
                 case "list":
-                    DataList.List( player );
-                    return true;
+                    DataList.List( player, 0 );
+                    break;
+                case "tenant":
+                    DataList.List( player, 1 );
+                    break;
                 case "info":
                     if ( RentData.RegionInfo( player, region ) ) {
-                        return true;
+                        break;
                     } else {
                         Tools.Prt( player, ChatColor.RED + "No Information.", Config.programCode );
-                        break;
+                        return false;
                     }
                 case "expired":
                     DataList.Expired( player );
-                    return true;
+                    break;
                 case "define":
-                    if ( ( !region.equals( "" ) ) && ( Tools.getUUID( username ) != null ) ) {
-                        Tools.Prt( player, "Manual Rent IN [" + region + "] " + username, Tools.consoleMode.full, Config.programCode );
-                        RentControl.in( player, Tools.getUUID( username ), username );
-                        return true;
-                    }
+                    if ( ( region.equals( "" ) ) || ( Tools.getUUID( username ) == null ) ) { return false; }
+                    Tools.Prt( player, "Manual Rent IN [" + region + "] " + username, Tools.consoleMode.full, Config.programCode );
+                    RentControl.in( player, Tools.getUUID( username ), username );
                     break;
                 case "undefine":
-                    if ( !region.equals( "" ) ) {
-                        Tools.Prt( player, "Manual Rent Out [" + region + "]", Tools.consoleMode.full, Config.programCode );
-                        RentControl.out( player );
-                        return true;
-                    }
+                    if ( region.equals( "" ) ) { return false; }
+                    Tools.Prt( player, "Manual Rent Out [" + region + "]", Tools.consoleMode.full, Config.programCode );
+                    RentControl.out( player );
                     break;
                 case "help":
+                    Tools.Prt( player, "=== Rental Areas Command Help ===", Config.programCode );
+                    Tools.Prt( player, "空室リスト      /rent vacancy", Config.programCode );
+                    Tools.Prt( player, "全部屋リスト    /rent list", Config.programCode );
+                    Tools.Prt( player, "入居者リスト    /rent tanant", Config.programCode );
+                    Tools.Prt( player, "期限切れリスト  /rent expired", Config.programCode );
+                    Tools.Prt( player, "部屋情報        /rent info [region]", Config.programCode );
+                    Tools.Prt( player, "手動入居処理    /rent defne [region] [player]", Config.programCode );
+                    Tools.Prt( player, "手動退去処理    /rent undefne [region]", Config.programCode );
+                    Tools.Prt( player, "システム状態    /rent stauts", Config.programCode );
+                    Tools.Prt( player, "設定再読込      /rent reload", Config.programCode );
+                    Tools.Prt( player, "表示モード切替  /rent console [max,full,normal,stop]", Config.programCode );
                     break;
                 default:
                     Tools.Prt( player, ChatColor.RED + "Unknown Command [" + args[0] + "]", Config.programCode );
-                    break;
+                    return false;
             }
         }
-        Tools.Prt( player, "=== Rental Areas Command Help ===", Config.programCode );
-        Tools.Prt( player, "/rent list", Config.programCode );
-        Tools.Prt( player, "/rent info [region]", Config.programCode );
-        Tools.Prt( player, "/rent expired", Config.programCode );
-        Tools.Prt( player, "/rent defne [region] [player]", Config.programCode );
-        Tools.Prt( player, "/rent undefne [region]", Config.programCode );
-        Tools.Prt( player, "/rent stauts", Config.programCode );
-        Tools.Prt( player, "/rent reload", Config.programCode );
-        Tools.Prt( player, "/rent console [max,full,normal,stop]", Config.programCode );
-        return false;
+        return true;
     }
 }
